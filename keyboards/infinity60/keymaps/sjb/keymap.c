4ef8c5b16d4db5670fb3edf617e7fab9c2909c2d
SjB <steve@sagacity.ca>
2017-08-16 07:59:50 -0400

    configure 60% keyboard to support OSX

--- keyboards/infinity60/keymaps/sjb/keymap.c
+++ keyboards/infinity60/keymaps/sjb/keymap.c
@@ -101,8 +101,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       TD_ESC,    KC_1,     KC_2,     KC_3,    KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,      KC_MINS,   KC_EQL,    KC_BSPC,  KC_BSPC,  \
       MT_TAB,    KC_Q,     KC_W,     KC_E,    KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,      TD_LBRC,   TD_RBRC,   TD_BSLS,  \
       OSM_LCTL,  KC_A,     KC_S,     KC_D,    KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,   TD_QUOT,   KC_ENT,    \
-      OSM_LSFT,  KC_Z,     KC_X,    KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,   OSM_LSFT,  M(BESC),   \
-      OSL_FNx,   OSL_NAV,  OSM_LALT,                    MT_SPC,                                           TD_LGUI,   OSM_HYPR,  OSM_MEH,   TD_TSKSWCH \
+      OSM_LSFT,  KC_Z,     KC_X,     KC_C,    KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,   OSM_LSFT,  M(BESC),   \
+      OSL_FNx,   OSL_NAV,  OSM_LALT,                    MT_SPC,                                           KC_LGUI,   OSM_HYPR,  OSM_MEH,   TD_TSKSWCH \
                      ),
   [FNx] = KEYMAP(
       RESET,    KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_MYCM,  F_TERM,   KC_CALC,  KC_WSCH,  KC_MAIL,   F_BROWSER, M(EDITOR), KC_DELT, KC_DELT,  \
--- keyboards/kc60/keymaps/sjb/keymap.c
+++ keyboards/kc60/keymaps/sjb/keymap.c
@@ -103,7 +103,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       MT_TAB,    KC_Q,     KC_W,     KC_E,    KC_R,     KC_T,     KC_Y,     KC_U,    KC_I,  KC_O,     KC_P,     TD_LBRC,  TD_RBRC,  TD_BSLS,  \
       OSM_LCTL,  KC_A,     KC_S,     KC_D,    KC_F,     KC_G,     KC_H,     KC_J,    KC_K,  KC_L,     KC_SCLN,  TD_QUOT,  KC_NO,    KC_ENT,   \
       OSM_LSFT,  KC_NO,    KC_Z,     KC_X,    KC_C,     KC_V,     KC_B,     KC_N,    KC_M,  KC_COMM,  KC_DOT,   KC_SLSH,  KC_NO,    OSM_LSFT,  \
-      OSL_FNx,   OSL_NAV,  OSM_LALT,                    KC_SPC,                             KC_NO,    TD_LGUI,  OSM_HYPR, OSM_MEH,  TD_TSKSWCH \
+      OSL_FNx,   OSL_NAV,  OSM_LALT,                    KC_SPC,                             KC_NO,    KC_LGUI,  OSM_HYPR, OSM_MEH,  TD_TSKSWCH \
                      ),
   [FNx] = KEYMAP(
       RESET,    KC_MPLY,  KC_MNXT,  KC_MUTE, KC_VOLD,  KC_VOLU,  KC_MYCM,   F_TERM,    KC_CALC, KC_WSCH,  KC_MAIL,   F_BROWSER, M(EDITOR), KC_DELT, \
