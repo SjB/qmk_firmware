f443c666207ef056a8533140d64a8ab4a8535aa1
SjB <steve@sagacity.ca>
2017-09-13 23:55:05 -0400

    swap the 0 and . in the Numpad layer

--- keyboards/ergodox/keymaps/sjb/keymap.c
+++ keyboards/ergodox/keymaps/sjb/keymap.c
@@ -221,7 +221,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |         |   `  |   /  |   |   |  [  |   ]  |      |           |      |  -   |   1  |   2  |   3  |  \   |        |
  * `---------+------+------+------+------+------+------'           `------+------+------+------+------+------+--------'
- *    |      |      |      |      |      |                                       |      |   .  |   0  |  =   |      |
+ *    |      |      |      |      |      |                                       |      |   0  |   .  |  =   |      |
  *    `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |      |      |       |      |      |
@@ -242,11 +242,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                              KC_TRNS,
                                            KC_TRNS, KC_TRNS, KC_TRNS,
        // right hand
-       KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,   RESET,
-       KC_NO,   KC_TILD, KC_7,    KC_8,    KC_9,  KC_ASTR, KC_DQT,
-                KC_SLSH, KC_4,    KC_5,    KC_6,  KC_PLUS, KC_QUOT,
-       KC_NO,   KC_MINS, KC_1,    KC_2,    KC_3,  KC_EQL, KC_TRNS,
-                         KC_TRNS, KC_DOT,  KC_0,  KC_TRNS, KC_TRNS,
+       KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   RESET,
+       KC_NO,   KC_TILD, KC_7,    KC_8,    KC_9,    KC_ASTR, KC_DQT,
+                KC_SLSH, KC_4,    KC_5,    KC_6,    KC_PLUS, KC_QUOT,
+       KC_NO,   KC_MINS, KC_1,    KC_2,    KC_3,    KC_EQL,  KC_TRNS,
+                         KC_TRNS, KC_0,    KC_DOT,  KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS,
        KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS
@@ -287,7 +287,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        TD_GRV,    KC_PGUP,  KC_7,        KC_8,      KC_9,       KC_ASTR, KC_TRNS,
                   KC_PGDN,  KC_4,        KC_5,      KC_6,       KC_PLUS, KC_TRNS,
        KC_EQUAL,  KC_GRV,   KC_1,        KC_2,      KC_3,       MT_BSLS, KC_TRNS,
-                            KC_TRNS,     KC_DOT,    KC_0,       KC_EQL,  KC_TRNS,
+                            KC_TRNS,     KC_0,      KC_DOT,     KC_EQL,  KC_TRNS,
        KC_TRNS,   KC_TRNS,
        KC_TRNS,
        KC_TRNS,   KC_TRNS,  KC_TRNS
