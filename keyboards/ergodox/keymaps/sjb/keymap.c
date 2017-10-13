c20f7d9307e3a0dce0d44643620a6d2c2c70c0f4
SjB <steve@sagacity.ca>
2017-10-13 08:56:18 -0400

    remove some tap dance keys.
    when using shift and a key that is configured with a tap dance even if
    the key was not tap twice.

--- keyboards/ergodox/keymaps/sjb/keymap.c
+++ keyboards/ergodox/keymaps/sjb/keymap.c
@@ -131,18 +131,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
         KC_ESC,   KC_1,      KC_2,       KC_3,    KC_4,       KC_5,     TD_TERM,
-        MT_TAB,   TD_Q,      KC_W,       KC_E,    KC_R,       KC_T,     TD_BSLS,
+        MT_TAB,   TD_Q,      KC_W,       KC_E,    KC_R,       KC_T,     KC_BSLS,
         OSM_LCTL, KC_A,      KC_S,       KC_D,    KC_F,       KC_G,
-        OSM_LSFT, MT_Z,      KC_X,       KC_C,    KC_V,       KC_B,     TD_LBRC,
+        OSM_LSFT, MT_Z,      KC_X,       KC_C,    KC_V,       KC_B,     KC_LBRC,
         OSL_NAV,  MT_TAB,    TD_TSKSWCH, MT_ESC, OSM_LALT,
                                                               KC_HOME,  KC_END,
                                                                         TG_NORMAN,
                                                    OSM_LCTL,  KC_BSPC,  TD_ESC,
         // right hand
         KC_MYCM,  KC_6,      KC_7,       KC_8,      KC_9,      KC_0,     KC_MINS,
-        TD_GRV,   KC_Y,      KC_U,       KC_I,      KC_O,      KC_P,     KC_EQL,
+        KC_GRV,   KC_Y,      KC_U,       KC_I,      KC_O,      KC_P,     KC_EQL,
                   KC_H,      KC_J,       KC_K,      KC_L,      KC_SCLN,  KC_QUOT,
-        TD_RBRC,  KC_N,      KC_M,       KC_COMM,   KC_DOT,    MT_SLSH,  OSM_RSFT,
+        KC_RBRC,  KC_N,      KC_M,       KC_COMM,   KC_DOT,    MT_SLSH,  OSM_RSFT,
                              KC_LGUI,    KC_MINS,   TD_QUOT,   OSM_MEH,  OSL_NUM,
         KC_PGUP,  KC_PGDN,
         TG_ADORE,
