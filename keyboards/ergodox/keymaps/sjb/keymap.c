039a20c680844916263cbfb6bc6fe38e61c699a6
SjB <steve@sagacity.ca>
2017-11-03 14:13:29 -0400

    new layout for default layer
    Just got my new Model 01, Keyboard and I'm trying to match my layouts

--- keyboards/ergodox/keymaps/sjb/keymap.c
+++ keyboards/ergodox/keymaps/sjb/keymap.c
@@ -40,6 +40,7 @@
 
 #define OSL_NAV OSL(NAV)
 #define OSL_NUM OSL(NUMPAD)
+#define TG_NUM TG(NUMPAD)
 #define OSM_LSFT OSM(MOD_LSFT)
 #define OSM_RSFT OSM(MOD_LSFT)
 #define OSM_LCTL OSM(MOD_LCTL)
@@ -108,54 +109,55 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   ESC  |   1  |   2  |   3  |   4  |   5  | Term |           | MYCM |   6  |   7  |   8  |   9  |   0  |   -    |
+ * |   Num  |   1  |   2  |   3  |   4  |   5  | Term |           | MYCM |   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | TAB    |   Q  |   W  |   E  |   R  |   T  |  \   |           |  `   |   Y  |   U  |   I  |   O  |   P  |   =    |
+ * |    \   |   Q  |   W  |   E  |   R  |   T  |  Tab |           |  Esc |   Y  |   U  |   I  |   O  |   P  |   =    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | LCTRL  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
+ * |    `   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
  * |--------+------+------+------+------+------|  [   |           |  ]   |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | NAV  |  TAB | SWCH |  ESC |  Alt |                                       | LGUI |   -  |   '  |  MEH | NUM  |
+ *   | NAV  |  NUM | SWCH |  TAB | CTRL |                                       | CTRL |   -  |   '  |  NUM | NAV  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | HOME | END  |       | PGUP | PGDN |
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | NRMN |       | ADORE|        |      |
- *                                 | LCTRL| BKSP |------|       |------|  ENTER | [ ]  |
- *                                 |      |      | Esc  |       | SWCH |        |      |
+ *                                 | LALT | BKSP |------|       |------|  ENTER | [ ]  |
+ *                                 |      |      | Esc  |       | LGUI |        |      |
  *                                 `--------------------'       `----------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
 // Otherwise, it needs KC_*
+    // MT_TAB
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_ESC,   KC_1,      KC_2,       KC_3,    KC_4,       KC_5,     TD_TERM,
-        MT_TAB,   TD_Q,      KC_W,       KC_E,    KC_R,       KC_T,     KC_BSLS,
-        OSM_LCTL, KC_A,      KC_S,       KC_D,    KC_F,       KC_G,
+        TG_NUM,   KC_1,      KC_2,       KC_3,    KC_4,       KC_5,     TD_TERM,
+        KC_BSLS,  TD_Q,      KC_W,       KC_E,    KC_R,       KC_T,     MT_TAB,
+        KC_GRV,   KC_A,      KC_S,       KC_D,    KC_F,       KC_G,
         OSM_LSFT, MT_Z,      KC_X,       KC_C,    KC_V,       KC_B,     KC_LBRC,
-        OSL_NAV,  MT_TAB,    TD_TSKSWCH, MT_ESC, OSM_LALT,
+        OSL_NAV,  OSL_NUM,   TD_TSKSWCH, MT_TAB,  OSM_LCTL,
                                                               KC_HOME,  KC_END,
                                                                         TG_NORMAN,
-                                                   OSM_LCTL,  KC_BSPC,  TD_ESC,
+                                                   OSM_LALT,  KC_BSPC,  TD_ESC,
         // right hand
         KC_MYCM,  KC_6,      KC_7,       KC_8,      KC_9,      KC_0,     KC_MINS,
-        KC_GRV,   KC_Y,      KC_U,       KC_I,      KC_O,      KC_P,     KC_EQL,
+        TD_ESC,   KC_Y,      KC_U,       KC_I,      KC_O,      KC_P,     KC_EQL,
                   KC_H,      KC_J,       KC_K,      KC_L,      KC_SCLN,  KC_QUOT,
         KC_RBRC,  KC_N,      KC_M,       KC_COMM,   KC_DOT,    MT_SLSH,  OSM_RSFT,
-                             KC_LGUI,    KC_MINS,   TD_QUOT,   OSM_MEH,  OSL_NUM,
+                             OSM_LCTL,   KC_MINS,   TD_QUOT,   OSL_NUM,  OSL_NAV,
         KC_PGUP,  KC_PGDN,
         TG_ADORE,
-        TD_TSKSWCH,   KC_ENT,    KC_SPC
+        TD_LGUI,  KC_ENT,    KC_SPC
     ),
 /* Keymap 1: Motion Layer
  *
  * ,---------------------------------------------------.           ,--------------------------------------------------.
  * |         |  F1  |  F2  |  F3  |  F4  |  F5  | Mute |           | App  |  F6  |  F7  |  F8  |  F9  |  F10 |   F11  |
  * |---------+------+------+------+------+------+------|           |------+------+------+------+------+------+--------|
- * |         |  F1  |  F2  |  F3  |  F4  | Mute | Vol+ |           | Dbg  | Home | PGDN | PGUP |  END | PRSC |   F12  |
+ * | Max     |  F1  |  F2  |  F3  |  F4  | Mute | Vol+ |           | Dbg  | Home | PGDN | PGUP |  END | PRSC |   F12  |
  * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | CAPSLCK |  F5  |  F6  |  F7  |  F8  | Vol+ |------|           |------| Left | Down |  Up| Rght | INS  |        |
+ * | CAPSLCK |  F5  |  F6  |  F7  |  F8  | Vol+ |------|           |------| Left | Down |  Up  | Rght | INS  |        |
  * |---------+------+------+------+------+------| Vol- |           | KDBG |------+------+------+------+------+--------|
  * |         |  F9  |  F10 |  F11 |  F12 | Vol- |      |           |      | Calc | SRCH | Mail | WWW  | RESET|        |
  * `---------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
@@ -172,7 +174,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Navigation
 [NAV] = KEYMAP(
        // left hand
-       RESET,     KC_F1,    KC_F2,       KC_F3,     KC_F4,      KC_F5,          KC_TRNS,
+       KC_TRNS,   KC_F1,    KC_F2,       KC_F3,     KC_F4,      KC_F5,          RESET,
        F_MAX,     KC_F1,    KC_F2,       KC_F3,     KC_F4,      KC_MUTE,        KC_TRNS,
        KC_CAPS,   KC_F5,    KC_F6,       KC_F7,     KC_F8,      KC_VOLU,
        KC_TRNS,   KC_F9,    KC_F10,      KC_F11,    KC_F12,     KC_VOLD,        KC_TRNS,
@@ -181,10 +183,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                                 KC_TRNS,
                                                     KC_TRNS,    KC_TRNS,        KC_TRNS,
        // right hand
-       KC_APP,    KC_F6,     KC_F7,      KC_F8,     KC_F9,      KC_F10,     KC_F11,
+       RESET,     KC_F6,     KC_F7,      KC_F8,     KC_F9,      KC_F10,     KC_F11,
        DEBUG,     KC_HOME,   KC_PGDN,    KC_PGUP,   KC_END,     KC_PSCR,    KC_F12,
                   KC_LEFT,   KC_DOWN,    KC_UP,     KC_RGHT,    KC_INS,     KC_NO,
-       M(KDBG),   KC_CALC,   KC_WSCH,    KC_MAIL,   F_BROWSER,  RESET,      KC_TRNS,
+       M(KDBG),   KC_CALC,   KC_WSCH,    KC_MAIL,   F_BROWSER,  KC_APP,     KC_TRNS,
                              KC_TRNS,    KC_TRNS,   KC_TRNS,    KC_TRNS,    KC_TRNS,
        KC_PWR,    KC_SLEP,
        KC_TRNS,
@@ -217,12 +219,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_VOLU,
        KC_CAPS, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
        KC_TRNS, KC_GRV,  KC_BSLS, KC_PIPE, KC_LBRC, KC_RBRC, KC_VOLD,
-                KC_TRNS, F_MAX,   KC_TRNS, KC_TRNS, KC_TRNS,
+                KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                                                     KC_TRNS, KC_TRNS,
                                                              KC_TRNS,
                                            KC_TRNS, KC_DELT, KC_TRNS,
        // right hand
-       KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   RESET,
+       KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS,
        KC_NO,   KC_TILD, KC_7,    KC_8,    KC_9,    KC_ASTR, KC_DQT,
                 KC_SLSH, KC_4,    KC_5,    KC_6,    KC_PLUS, KC_QUOT,
        KC_NO,   KC_MINS, KC_1,    KC_2,    KC_3,    KC_EQL,  KC_TRNS,
