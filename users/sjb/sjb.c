25b24f904257489fcd469259c101a26e15c92eed
sjb <steve@sagacity.ca>
2022-03-24 02:27:41 -0400

    fix: layer_state_set_keymap should get called after we know what layer we are on

--- users/sjb/sjb.c
+++ users/sjb/sjb.c
@@ -23,7 +23,6 @@ layer_state_t layer_state_set_keymap(layer_state_t state) {
 }
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-    state = layer_state_set_keymap(state);
 
     if (IS_LAYER_ON_STATE(state, _NUMPAD)
         && IS_LAYER_OFF_STATE(state, _LOWER)
@@ -31,5 +30,6 @@ layer_state_t layer_state_set_user(layer_state_t state) {
         return state;
     }
 
-    return update_tri_layer_state(state, _LOWER,  _RAISE, _NUMPAD);
+    state = update_tri_layer_state(state, _LOWER,  _RAISE, _NUMPAD);
+    return layer_state_set_keymap(state);
 }
