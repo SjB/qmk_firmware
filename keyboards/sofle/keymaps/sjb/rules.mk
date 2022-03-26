4a53db4e10826cfcae3fa6e9fda99e984d2fbdaf
sjb <steve@sagacity.ca>
2022-03-26 10:37:25 -0400

    fix: remove extra duplicate rules

--- keyboards/sofle/keymaps/sjb/rules.mk
+++ keyboards/sofle/keymaps/sjb/rules.mk
@@ -1,6 +1,9 @@
+# MCU name
+MCU = atmega32u4
+
+# Bootloader selection
+BOOTLOADER = atmel-dfu
+
 OLED_ENABLE = no
 OLED_DRIVER = SSD1306
 ENCODER_ENABLE = yes
-CONSOLE_ENABLE = no
-EXTRAKEY_ENABLE = yes
-NKRO_ENABLE = yes
