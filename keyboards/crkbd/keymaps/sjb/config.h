e4606912a75e29cf3aa348d73f9f2fc1ac825386
Steve Beaulac <steve@nca.uwo.ca>
2022-04-21 11:05:25 -0400

    feat: change oled function, make it look nicer

--- keyboards/crkbd/keymaps/sjb/config.h
+++ keyboards/crkbd/keymaps/sjb/config.h
@@ -26,7 +26,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     #define RGBLIGHT_EFFECT_BREATHING
     #define RGBLIGHT_EFFECT_RAINBOW_MOOD
     #define RGBLIGHT_EFFECT_TWINKLE
-//    #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+    #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
 //    #define RGBLIGHT_EFFECT_SNAKE
 //    #define RGBLIGHT_EFFECT_KNIGHT
 //    #define RGBLIGHT_EFFECT_CHRISTMAS
