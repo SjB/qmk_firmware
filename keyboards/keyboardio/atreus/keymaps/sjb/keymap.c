601b7ff1a50558ea4558d737f4f26af5a2b01b91
Steve Beaulac <steve@sagacity.ca>
2023-03-21 23:36:23 -0400

    fix: added left mouse thumb cluster to atreus keyboard

--- keyboards/keyboardio/atreus/keymaps/sjb/keymap.c
+++ keyboards/keyboardio/atreus/keymaps/sjb/keymap.c
@@ -54,6 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     LEFT_MOUSE_ROW_3 , _______ , _______ , RIGHT_MOUSE_ROW_3 ,
 
     KC_NO , KC_NO, NK_TOGG ,
+    LEFT_MOUSE_THUMB_CLUSTER(_QWERTY) ,
     RIGHT_MOUSE_THUMB_CLUSTER ,
     KC_NO , KC_NO , KC_NO
   ),
